76df821f9ebc0564d28e112403f1f005e4ed8ab8
cutegirl789 <babycute234@zohomail.com>
2024-12-08 22:20:48 -0800

    Update index.js

--- files/index.js
+++ files/index.js
@@ -6,8 +6,8 @@ const UUID = process.env.UUID || 'fcf28e64-77a9-4cab-87f6-b1a3a82c5b7a'; //若�
 const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nz.f4i.cn';     
 const NEZHA_PORT = process.env.NEZHA_PORT || '5555';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || 'N9BVoBfucVIrIzCBt8';   // 没用哪吒，把这个参数空着
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'choreo22.cmksleuubxgf.us.kg';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiMmY4NzI4OTk5Y2U0ZTRiMmIyMGFiYzViZmU5Y2M5ZWUiLCJ0IjoiODI2N2Q5MTUtYTNkZS00YTY2LTliYmItNDQyNmY0NTg0N2VhIiwicyI6IllXVmhOV1UxWlRFdE1UQmlaaTAwTlRaa0xUazRNek10TnprellqUXhPR1ZsTlROaiJ9';
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || '3cfdhuk.erjidhsnb.us.kg';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiMmY4NzI4OTk5Y2U0ZTRiMmIyMGFiYzViZmU5Y2M5ZWUiLCJ0IjoiYzJjMTUxNGMtNDA1Mi00MzZlLTk2MjUtMzkwYjQ0MzEwOWJhIiwicyI6Ik1EVTNaREUyT0RNdE5tRTFaaTAwTVRoakxXSmtZbVl0TkRsaE5XUTJPVEZpTXpGaiJ9';
 const CFIP = process.env.CFIP || 'na.ma';
 const NAME = process.env.NAME || 'Choreo';
 
