509f68c24b7f84e8cfd55669d7a39b41565ad4c2
cutegirl789 <babycute234@zohomail.com>
2024-12-08 22:09:27 -0800

    Update index.js

--- files/index.js
+++ files/index.js
@@ -7,7 +7,7 @@ const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nz.f4i.cn';
 const NEZHA_PORT = process.env.NEZHA_PORT || '5555';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || 'N9BVoBfucVIrIzCBt8';   // 没用哪吒，把这个参数空着
 const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'choreo22.cmksleuubxgf.us.kg';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiMmY4NzI4OTk5Y2U0ZTRiMmIyMGFiYzViZmU5Y2M5ZWUiLCJ0IjoiODFlMzE2ZmMtNTA2ZS00YWYzLThhZmUtYzI2NzA5YWU5ODQyIiwicyI6IllURm1ORGM1WmpFdFptWmlNeTAwWTJGa0xUbGtOVE10WTJWaU5qZzRPRE5qTXpSayJ9';
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiMmY4NzI4OTk5Y2U0ZTRiMmIyMGFiYzViZmU5Y2M5ZWUiLCJ0IjoiODI2N2Q5MTUtYTNkZS00YTY2LTliYmItNDQyNmY0NTg0N2VhIiwicyI6IllXVmhOV1UxWlRFdE1UQmlaaTAwTlRaa0xUazRNek10TnprellqUXhPR1ZsTlROaiJ9';
 const CFIP = process.env.CFIP || 'na.ma';
 const NAME = process.env.NAME || 'Choreo';
 
